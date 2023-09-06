5957d932de5537603245806aea6f3cbb910d2f4a
romain pelletant <romain.pelletant@fullfreqs.com>
2023-09-07 00:07:07 +0200

    hs: add heatshrink configuration settings
    Compression settings could be changed
    Default values: window_sz2 10 and lookahead_sz2 4
    
    Signed-off-by: romain pelletant <romain.pelletant@fullfreqs.com>

---
+++ CHANGELOG.md
@@ -0,0 +1,26 @@
+# Change Log
+All notable changes to this project will be documented in this file.
+Release available on [PyPI](https://pypi.org/project/bsdiffhs/)
+The format is based on [Keep a Changelog](http://keepachangelog.com/)
+and this project adheres to [Semantic Versioning](http://semver.org/).
+ 
+## [0.2.0] - 2023-09-30
+
+### Added
+
+- add window_sz2 and lookahead_sz2 heatshrink parameters for compression/decompression
+- add test for supported range parameters
+
+### Changed
+ 
+### Fixed
+ 
+## [0.1.0] - 2022-08-23
+ 
+### Added
+
+- Initial commit
+   
+### Changed
+ 
+### Fixed
--- bsdiffhs/format.py
+++ bsdiffhs/format.py
@@ -9,13 +9,15 @@ if sys.version_info[0] == 2:
 else:
     from io import BytesIO
 
-MAGIC = b'BSDIFF40'
-
+MAGIC = b'BSDIFFHS'
+DEFAULT_WINDOW_SZ2 = 10
+DEFAULT_LOOKAHEAD_SZ2 = 4
+INPUT_BUFFER_SIZE = 16384
 
 import bsdiffhs.core as core
 
 
-def write_patch(fo, len_dst, tcontrol, bdiff, bextra):
+def write_patch(fo, len_dst, tcontrol, bdiff, bextra, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
     """write a BSDIFFHS-format patch to stream 'fo'
     """
     fo.write(MAGIC)
@@ -25,9 +27,9 @@ def write_patch(fo, len_dst, tcontrol, bdiff, bextra):
         for x in c:
             faux.write(core.encode_int64(x))
     # compress each block
-    bcontrol = heatshrink2.compress(faux.getvalue())
-    bdiff = heatshrink2.compress(bdiff)
-    bextra = heatshrink2.compress(bextra)
+    bcontrol = heatshrink2.compress(faux.getvalue(), window_sz2, lookahead_sz2)
+    bdiff = heatshrink2.compress(bdiff, window_sz2, lookahead_sz2)
+    bextra = heatshrink2.compress(bextra, window_sz2, lookahead_sz2)
     for n in len(bcontrol), len(bdiff), len_dst:
         fo.write(core.encode_int64(n))
     fo.write(bcontrol)
@@ -35,18 +37,18 @@ def write_patch(fo, len_dst, tcontrol, bdiff, bextra):
     fo.write(bextra)
 
 
-def read_patch(fi, header_only=False):
-    """read a BSDIFF4-format patch from stream 'fi'
+def read_patch(fi, header_only=False, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
+    """read a BSDIFFHS-format patch from stream 'fi'
     """
     magic = fi.read(8)
     if magic[:7] != MAGIC[:7]:
-        raise ValueError("incorrect magic bsdiff4 header")
+        raise ValueError("incorrect magic bsdiffhs header")
     # length headers
     len_control = core.decode_int64(fi.read(8))
     len_diff = core.decode_int64(fi.read(8))
     len_dst = core.decode_int64(fi.read(8))
     # read the control header
-    bcontrol = heatshrink2.decompress(fi.read(len_control))
+    bcontrol = heatshrink2.decompress(fi.read(len_control), INPUT_BUFFER_SIZE, window_sz2, lookahead_sz2)
     tcontrol = [(core.decode_int64(bcontrol[i:i + 8]),
                  core.decode_int64(bcontrol[i + 8:i + 16]),
                  core.decode_int64(bcontrol[i + 16:i + 24]))
@@ -54,8 +56,8 @@ def read_patch(fi, header_only=False):
     if header_only:
         return len_control, len_diff, len_dst, tcontrol
     # read the diff and extra blocks
-    bdiff = heatshrink2.decompress(fi.read(len_diff))
-    bextra = heatshrink2.decompress(fi.read())
+    bdiff = heatshrink2.decompress(fi.read(len_diff), INPUT_BUFFER_SIZE, window_sz2, lookahead_sz2)
+    bextra = heatshrink2.decompress(fi.read(), INPUT_BUFFER_SIZE, window_sz2, lookahead_sz2)
     return len_dst, tcontrol, bdiff, bextra
 
 
@@ -65,59 +67,59 @@ def read_data(path):
     return data
 
 
-def diff(src_bytes, dst_bytes):
+def diff(src_bytes, dst_bytes, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
     """diff(src_bytes, dst_bytes) -> bytes
 
-    Return a BSDIFF4-format patch (from src_bytes to dst_bytes) as bytes.
+    Return a BSDIFFHS-format patch (from src_bytes to dst_bytes) as bytes.
     """
     faux = BytesIO()
-    write_patch(faux, len(dst_bytes), *core.diff(src_bytes, dst_bytes))
+    write_patch(faux, len(dst_bytes), *core.diff(src_bytes, dst_bytes), window_sz2, lookahead_sz2)
     return faux.getvalue()
 
 
-def file_diff(src_path, dst_path, patch_path):
+def file_diff(src_path, dst_path, patch_path, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
     """file_diff(src_path, dst_path, patch_path)
 
-    Write a BSDIFF4-format patch (from the file src_path to the file dst_path)
+    Write a BSDIFFHS-format patch (from the file src_path to the file dst_path)
     to the file patch_path.
     """
     src = read_data(src_path)
     dst = read_data(dst_path)
     with open(patch_path, 'wb') as fo:
-        write_patch(fo, len(dst), *core.diff(src, dst))
+        write_patch(fo, len(dst), *core.diff(src, dst), window_sz2, lookahead_sz2)
 
 
-def patch(src_bytes, patch_bytes):
+def patch(src_bytes, patch_bytes, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=4):
     """patch(src_bytes, patch_bytes) -> bytes
 
-    Apply the BSDIFF4-format patch_bytes to src_bytes and return the bytes.
+    Apply the BSDIFFHS-format patch_bytes to src_bytes and return the bytes.
     """
-    return core.patch(src_bytes, *read_patch(BytesIO(patch_bytes)))
+    return core.patch(src_bytes, *read_patch(BytesIO(patch_bytes), window_sz2=window_sz2, lookahead_sz2=lookahead_sz2))
 
 
-def file_patch_inplace(path, patch_path):
+def file_patch_inplace(path, patch_path, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
     """file_patch_inplace(path, patch_path)
 
-    Apply the BSDIFF4-format file patch_path to the file 'path' in place.
+    Apply the BSDIFFHS-format file patch_path to the file 'path' in place.
     """
     with open(patch_path, 'rb') as fi:
         with open(path, 'r+b') as fo:
             data = fo.read()
             fo.seek(0)
-            fo.write(core.patch(data, *read_patch(fi)))
+            fo.write(core.patch(data, *read_patch(fi, window_sz2=window_sz2, lookahead_sz2=lookahead_sz2)))
             fo.truncate()
 
 
-def file_patch(src_path, dst_path, patch_path):
+def file_patch(src_path, dst_path, patch_path, window_sz2=DEFAULT_WINDOW_SZ2, lookahead_sz2=DEFAULT_LOOKAHEAD_SZ2):
     """file_patch(src_path, dst_path, patch_path)
 
-    Apply the BSDIFF4-format file patch_path to the file src_path and
+    Apply the BSDIFFHS-format file patch_path to the file src_path and
     write the result to the file dst_path.
     """
     from os.path import abspath
 
     if abspath(dst_path) == abspath(src_path):
-        file_patch_inplace(src_path, patch_path)
+        file_patch_inplace(src_path, patch_path, window_sz2, lookahead_sz2)
         return
 
     with open(patch_path, 'rb') as fi:
